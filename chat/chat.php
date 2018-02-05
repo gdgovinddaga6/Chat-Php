412102e76cef64a2cde212be12bd1d0fb0dbb9c3
govind daga <gdgovinddaga6@gmail.com>
2018-02-05 18:49:16 +0530

    Add files via upload

---
+++ chat/chat.php
@@ -0,0 +1,12 @@
+<?php
+	include 'database.php';
+	$query = "SELECT * FROM chat ORDER BY id DESC";
+	$run = $con->query($query);
+		while($row = $run->fetch_array()):
+	?>	
+		<div id ="chat_data">
+		<span style = "color:green"><?php echo $row['name'];?></span>:
+			<span style = "color:aqua"><?php echo $row['msg'];?></span>
+			<span style= "float:right"><?php echo formatDate($row['date']);?></span>
+		</div>
+		<?php endwhile;?>
---
+++ chat/index.php
@@ -0,0 +1,98 @@
+<?php
+include 'database.php'; 
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>chat system</title>
+	<link rel="stylesheet" href="style.css" media="all"/>
+	<script>
+			function ajax(){
+				
+			
+			var req  = new XMLHttpRequest();
+			req.onreadystatechange = function(){
+				if(req.readyState == 4 && req.status == 200){
+					document.getElementById('chat').innerHTML = req.responseText;
+				}	
+				}
+			req.open('GET','chat.php',true);
+			req.send();
+			
+			}
+			setInterval(function(){ajax()},1000);
+	</script>
+	<style> 
+#myDIV {
+    width: 1920x;
+    height: 1080px;
+    background: red;
+    -webkit-animation: mymove 5s infinite; 
+    animation: mymove 5s infinite;
+}
+
+
+@-webkit-keyframes mymove {
+    from {background-color: red;}
+    to {background-color: blue;}
+}
+
+
+@keyframes mymove {
+    from {background-color: red;}
+    to {background-color: blue;}
+}
+</style>
+</head>
+
+<body onload="ajax();">
+
+<div id ="container">
+	<div id="chat_box">
+	<div id="chat"></div>
+	<div id="myDIV"></div>
+	
+	
+	</div>
+			<form method ="post" action = "index.php">
+			
+				<input type="text" name="name" placeholder="Enter name">
+				<textarea name ="msg" placeholder="Enter any message"></textarea>
+				<input type="submit" name="submit" value ="SEND">
+			</form>
+
+
+
+			<?php
+			
+			/*$name = mysqli_real_escape_string($con, $_REQUEST['name']);
+			$msg = mysqli_real_escape_string($con, $_REQUEST['msg']);
+
+			$sql = "INSERT INTO chat (name,msg) VALUES ('name', 'msg')";*/
+			
+			
+			
+			
+			
+			
+			
+			
+			
+				if(isset($_POST ['submit'])) 
+				{
+					$name = $_POST['name'];
+					$msg = $_POST['msg'];
+					$query = "INSERT INTO chat (name,msg) VALUES ('$name','$msg')";
+					
+					$run = $con->query($query);
+					
+						if($run) {
+					
+						echo "<embed loop ='false' src = 'chat.wav' hidden ='true' autoplay ='true'/>";
+					}
+					}
+			 
+			?>
+</div>
+</body>
+</html>
